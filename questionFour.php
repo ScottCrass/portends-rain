757f222e9230a8b8de0f5a87a70030585403d712
Scott Crass <scott.crass@gmail.com>
2015-08-19 13:32:29 -0800

    Update questionFour.php

--- questionFour.php
+++ questionFour.php
@@ -5,13 +5,24 @@ function ackermann( $m , $n )
     {
         return $n + 1;
     }
+    //optimize for low lavues of M. 
+    elseif ($m==1)
+  	{
+         return $n + 2;
+  	}   
+  	elseif ($m==2)
+  	{
+         return 3 + 2 * $n;
+  	}   
+  	elseif ($m==3)
+  	{
+         return 5 + 8 * (2**$n-1);
+  	}   
     elseif ( $n==0 )
     {
         return ackermann( $m-1 , 1 );
     }
     return ackermann( $m-1, ackermann( $m , $n-1 ) );
 }
- 
-echo ackermann( 3, 5 );
-//http://codepad.viper-7.com/hK7Evs
+echo ackermann( 3, 4 );
 ?>
