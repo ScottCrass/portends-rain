0c9a06f84a8706588cf356abbff63b30d8ba1e92
Scott Crass <scott.crass@gmail.com>
2015-08-18 20:59:32 -0800

    Update questionThree.php

--- questionThree.php
+++ questionThree.php
@@ -1,19 +1,17 @@
 <?php
 function Palindrome($string) {
-
+  //we have finished. It is a palindrome.
     if ((strlen($string) == 1) || (strlen($string) == 0)) {
         echo "This is a palindrome";
     }
-
     else {
-
+  //if the string is equal on both ends call recurse one character further in on each end
         if (substr($string,0,1) == substr($string,(strlen($string) - 1),1)) {
             return Palindrome(substr($string,1,strlen($string) -2));
         }
+  //Or its not a palindrome. Go home.
         else { echo "String is not a palindrome, sorry."; }
     }
 }
-
-
 Palindrome("tst");
 ?>
