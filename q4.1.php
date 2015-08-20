58c6a3fc1fe74691c675a5cfb3f9b3da987106fa
Scott Crass <scott.crass@gmail.com>
2015-08-20 09:30:22 -0800

    Update q4.1.php

--- q4.1.php
+++ q4.1.php
@@ -11,13 +11,36 @@ $memoize = function($func)
         if ( ! isset($cache[$key])) {
             $cache[$key] = call_user_func_array($func, $args);
         }
+
         return $cache[$key];
     };
 };
 
 $ackermann = $memoize(function($m,$n) use (&$ackermann)
-                     {
-                       return $ackermann( $m-1, $ackermann( $m , $n-1 ) );
-                     }); 
-echo $ackermann(2,1);
+{
+    if ( $m==0 )
+    {
+        return $n + 1;
+    }
+    //optimize for low lavues of M. 
+    elseif ($m==1)
+  	{
+         return $n + 2;
+  	}   
+  	elseif ($m==2)
+  	{
+         return 3 + 2 * $n;
+  	}   
+  	elseif ($m==3)
+  	{
+         return 5 + 8 * (2**$n-1);
+  	}   
+    elseif ( $n==0 )
+    {
+        return ackermann( $m-1 , 1 );
+    }
+    return ackermann( $m-1, ackermann( $m , $n-1 ) );
+});
+
+echo $ackermann(3, 4);
 ?>
